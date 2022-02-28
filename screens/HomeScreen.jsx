2bb67e15464e894f24cb4dd6f6f8bc7fa6278e29
Leon N. Dela Pena <leonndp@gmail.com>
2022-02-28 11:02:49 +0800

    Added NavOptions component

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,19 +1,38 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  View,
+  StatusBar,
+  Image,
+} from "react-native";
 import React from "react";
-import SafeViewAndroid from "../components/SafeViewAndroid";
+import tw from "twrnc";
+import NavOptions from "../components/NavOptions";
 
 const HomeScreen = () => {
   return (
-    <SafeAreaView style={SafeViewAndroid.AndroidSafeArea}>
-      <Text style={styles.text}>HomeScreen</Text>
+    <SafeAreaView
+      style={tw`h-full bg-white android:pt-[${StatusBar.currentHeight}]`}
+    >
+      <View style={tw`p-5`}>
+        <Image
+          style={{
+            width: 100,
+            height: 100,
+            resizeMode: "contain",
+          }}
+          source={{
+            uri: "https://links.papareact.com/gzs",
+          }}
+        />
+
+        <NavOptions />
+      </View>
     </SafeAreaView>
   );
 };
 
 export default HomeScreen;
 
-const styles = StyleSheet.create({
-  text: {
-    color: "red",
-  },
-});
+const styles = StyleSheet.create({});
